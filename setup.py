b4b60b463ddff95718e3ca34e0434ffcfaed7dd5
bruno-polimi <bru1uno2@gmail.com>
2024-02-15 19:32:14 +0100

    updated the version of the model

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='bayesian_multitarget_latent_factors',
-    version='0.1.0',
+    version='0.2.0',
     packages=find_packages(),
     package_data={
         # If any package contains *.stan files, include them:
